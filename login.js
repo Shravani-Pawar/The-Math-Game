f0521f4c06ea0135027f2fd82ba100674aace6d0
Shravani-Pawar <89009083+Shravani-Pawar@users.noreply.github.com>
2022-01-23 17:22:04 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+
+function addUser()
+{
+    localStorage.setItem("User1",document.getElementById("player1_name_input").value);
+    localStorage.setItem("User2",document.getElementById("player2_name_input").value);
+    window.location="mainfile.html";
+}
